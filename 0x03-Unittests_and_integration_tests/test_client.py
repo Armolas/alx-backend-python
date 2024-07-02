07408d69185a4e095e3fcec011e9621d09c8e5d9
Armolas <armolas06@gmail.com>
2024-07-02 06:22:52 +0100

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''test for client'''
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -23,4 +23,40 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client.org
         
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
-        self.assertEqual(result, {"org": org_name}) 
+        self.assertEqual(result, {"org": org_name})
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        '''tests the public repo urls'''
+        expected_repos_url = "https://api.github.com/orgs/google/repos"
+        mock_org.return_value = {"repos_url": expected_repos_url} 
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+        self.assertEqual(result, expected_repos_url)
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        '''tests the public repos'''
+        expected_repos_url = "https://api.github.com/orgs/google/repos"
+        mock_public_repos_url.return_value = expected_repos_url
+        mock_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+        mock_get_json.return_value = mock_payload
+        client = GithubOrgClient("google")
+        result = client.public_repos()
+        self.assertEqual(result, ["repo1", "repo2", "repo3"])
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(expected_repos_url)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        '''tests if a github client has license'''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
